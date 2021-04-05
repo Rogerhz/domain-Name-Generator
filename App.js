397af1738e5ecdff8a947ba7d1f197c81e33ef1e
Rogerhz <rogerhernandez0260@gmail.com>
2021-04-05 17:44:53 -0400

    domineNameGenerator

---
+++ App.js
@@ -0,0 +1,20 @@
+let pronoun = ["our","the"];
+let adj = ["anxious","bad","attractive","angry"];
+let noun = ["Yonkleiverson","elpicure","hola","iphone"];
+let dotcom = [".com"]
+
+for (let pron of pronoun){
+    for (let adjj of adj){
+
+        for (let nou of noun){
+
+            for (let dot of dotcom){
+                  console.log(pron + adjj + nou + dot);
+            }
+
+        }
+
+
+    } 
+
+}
